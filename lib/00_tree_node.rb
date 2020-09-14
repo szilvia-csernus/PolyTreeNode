896e0edcd1d1326f982358f9fe3eb6ed24e3515f
szilvi <csernus.szilvi@gmail.com>
2020-09-14 11:15:39 +0100

    dfs method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -29,5 +29,14 @@ class PolyTreeNode
         child_node.parent = nil 
     end
 
+    def dfs(target_value)
+        return self if self.value == target_value
+        self.children.each do |child|
+            search_result = child.dfs(target_value)
+            return search_result unless search_result == nil
+        end
+        nil
+    end
+
 
 end
